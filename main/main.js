6532eda019a83f425f692d8a9c408ab0a5cdb668
BILL WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGBI2@OOCL.COM>
2019-07-10 18:21:46 +0800

    mission complete

--- main/main.js
+++ main/main.js
@@ -1,3 +1,57 @@
 'use strict';
+function getItemList(inputs){
+    var list = {}; 
+    
+    for(var i= 0, l = inputs.length; i< l; i++){ 
+        var item = inputs[i]; 
+        if(item.includes("-")){
+            let itemDetail = item.split("-");
+            list[itemDetail[0]]= list[itemDetail[0]] != null? (list[itemDetail[0]] + parseFloat(itemDetail[1])) : parseFloat(itemDetail[1]);
+        }else{
+            list[item] = (list[item] +1 ) || 1; 
+        }
+    } 
+    return list; 
+}
+function getBill(ItemList,allItems){
+  let receipt = `***<没钱赚商店>收据***\n`;
+  let total = 0; 
+  let save = 0;
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+  for(let key in ItemList){
+    allItems.forEach(i => {
+      if(key == i.barcode){
+        let itemPrice = 0;
+        itemPrice = checkPromotions(i.barcode,i.price,ItemList[key]);
+        receipt = receipt.concat(`名称：${i.name}，数量：${ItemList[key]}${i.unit}，单价：${i.price.toFixed(2)}(元)，小计：${itemPrice.sum.toFixed(2)}(元)\n`)
+        total += Number(itemPrice.sum);
+        save += Number(itemPrice.save);
+      }
+
+    })
+  }
+
+  receipt = receipt.concat(`----------------------\n总计：${total.toFixed(2)}(元)\n节省：${save.toFixed(2)}(元)\n**********************`);
+  return receipt
+}
+
+function checkPromotions(barcode,price,num){
+  let value = {sum:'',save:''}; 
+  let allPromotions = loadPromotions();
+  let bool  = allPromotions[0].barcodes.includes(barcode);
+  if(bool){
+    value.save = price * Math.floor(num / 3);
+    value.sum = price * num - value.save;
+  }else{
+    value.sum = price * num; 
+  }
+  return value;
+}
+function printReceipt(inputs) {
+  let allItems = loadAllItems();
+  let ItemList = getItemList(inputs);
+  let receipt = getBill(ItemList,allItems);
+  console.log(receipt)
+  return receipt;
+}
+module.exports = printReceipt;
