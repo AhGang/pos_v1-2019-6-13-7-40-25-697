0273728fa11da734f59585f5d4da0a378da1242e
AhGang <474494594@qq.com>
2019-07-21 16:09:49 +0800

    用filter,forEach 替换原来的for循环嵌套

--- main/main.js
+++ main/main.js
@@ -15,20 +15,15 @@ function getBill(ItemList,allItems){
   let receipt = `***<没钱赚商店>收据***\n`;
   let total = 0; 
   let save = 0;
-
-  for(let key in ItemList){
-    allItems.forEach(i => {
-      if(key == i.barcode){
-        let itemPrice = 0;
-        itemPrice = checkPromotions(i.barcode,i.price,ItemList[key]);
-        receipt = receipt.concat(`名称：${i.name}，数量：${ItemList[key]}${i.unit}，单价：${i.price.toFixed(2)}(元)，小计：${itemPrice.sum.toFixed(2)}(元)\n`)
-        total += Number(itemPrice.sum);
-        save += Number(itemPrice.save);
-      }
-
-    })
-  }
-
+  let keys = Object.keys(ItemList)
+  keys.forEach(key =>{
+    const item = allItems.filter(item => item.barcode == key)[0]
+    let itemPrice = 0;
+    itemPrice = checkPromotions(item.barcode,item.price,ItemList[key]);
+    receipt = receipt.concat(`名称：${item.name}，数量：${ItemList[key]}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${itemPrice.sum.toFixed(2)}(元)\n`)
+    total += Number(itemPrice.sum);
+    save += Number(itemPrice.save);
+  })
   receipt = receipt.concat(`----------------------\n总计：${total.toFixed(2)}(元)\n节省：${save.toFixed(2)}(元)\n**********************`);
   return receipt
 }
